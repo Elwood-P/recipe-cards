eaa8bebf99eb05e1bc2a2b9400fa10e02f2d13bb
Elwood-P <info@pjlittlewood.co.uk>
2022-06-21 20:00:35 +0100

    Update to match Provider

--- src/store/RecipeListContext.js
+++ src/store/RecipeListContext.js
@@ -2,8 +2,9 @@ import React from 'react';
 
 const RecipeListContext = React.createContext({
   recipes: [],
+  setRecipes: () => {},
   addRecipeHandler: () => {},
-  deleteRecipeHandler: (idToDelete) => {},
+  deleteRecipeHandler: () => {},
   isEditing: {},
   editRecipeHandler: () => {},
 });
